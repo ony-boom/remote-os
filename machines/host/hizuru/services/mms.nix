b45152ed168af309a1bd3f7087f6b4e8d939fc58
Ony <onyrakoto27@gmail.com>
2025-06-23 16:02:18 +0300

    updated flake

--- machines/host/hizuru/services/mms.nix
+++ machines/host/hizuru/services/mms.nix
@@ -1,12 +1,19 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.mms = {
     enable = true;
+    user = "ony";
     host = "0.0.0.0";
   };
 
-  services.caddy.virtualHosts = {
-    "music.ony.world".extraConfig = ''
-      reverse_proxy http://localhost:${builtins.toString config.services.mms.port}
-    '';
+  services.caddy.virtualHosts = lib.mkIf config.services.mms.enable {
+    "music.ony.world" = {
+      extraConfig = ''
+        reverse_proxy http://localhost:${builtins.toString config.services.mms.port}
+      '';
+    };
   };
 }
