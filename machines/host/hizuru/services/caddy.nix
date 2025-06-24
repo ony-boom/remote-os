5d3802ab53b8a6fb2f9ebc9374f988aa281a2d8e
Ony <onyrakoto27@gmail.com>
2025-06-24 18:14:20 +0300

    fix caddy redirect error

--- machines/host/hizuru/services/caddy.nix
+++ machines/host/hizuru/services/caddy.nix
@@ -3,19 +3,20 @@
   lib,
   ...
 }: {
-  services.caddy.virtualHosts =
+  services.caddy.virtualHosts = lib.mkMerge [
     {
       "devenv.ony.world" = {
         extraConfig = ''
-          redir https://raw.githubusercontent.com/ony-boom/home-manager/refs/heads/main/install.sh
+          redir / https://raw.githubusercontent.com/ony-boom/home-manager/refs/heads/main/install.sh 302
         '';
       };
     }
-    // lib.mkIf config.services.mms.enable {
+    (lib.mkIf config.services.mms.enable {
       "music.ony.world" = {
         extraConfig = ''
-          reverse_proxy http://localhost:${builtins.toString config.services.mms.port}
+          reverse_proxy http://localhost:${toString config.services.mms.port}
         '';
       };
-    };
+    })
+  ];
 }
