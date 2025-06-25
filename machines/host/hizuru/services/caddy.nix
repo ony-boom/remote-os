c6fa8441ee9dee9485366317ac393e6c5dbbea93
Ony <onyrakoto27@gmail.com>
2025-06-25 11:23:00 +0300

    moved devenv script redirect inside my portfolio

--- machines/host/hizuru/services/caddy.nix
+++ machines/host/hizuru/services/caddy.nix
@@ -4,13 +4,17 @@
   ...
 }: {
   services.caddy.virtualHosts = lib.mkMerge [
-    {
+    /*
+       // Now managed in ony.world/devenv-setup
+       // so no longer needed here, but for reference:
+       {
       "devenv.ony.world" = {
         extraConfig = ''
           redir / https://raw.githubusercontent.com/ony-boom/home-manager/refs/heads/main/install.sh 302
         '';
       };
     }
+    */
     (lib.mkIf config.services.mms.enable {
       "music.ony.world" = {
         extraConfig = ''
