ee4c3e29342ec7bc36fcd72179fe81e53fc98691
Ony <onyrakoto27@gmail.com>
2025-08-07 13:41:12 +0300

    added umami redirect

--- machines/host/hizuru/services/caddy.nix
+++ machines/host/hizuru/services/caddy.nix
@@ -2,28 +2,41 @@
   config,
   lib,
   ...
-}: {
-  services.caddy.virtualHosts = lib.mkMerge [
-    {
-      "ony.world" = {
-        extraConfig = ''
-          reverse_proxy http://localhost:4321
-        '';
-      };
+}: let
+  website = {
+    "ony.world" = {
+      extraConfig = ''
+        reverse_proxy http://localhost:4321
+      '';
+    };
 
-      # Redirect www.ony.world → ony.world
-      "www.ony.world" = {
-        extraConfig = ''
-          redir https://ony.world{uri} permanent
-        '';
-      };
-    }
-    (lib.mkIf config.services.mms.enable {
-      "music.ony.world" = {
-        extraConfig = ''
-          reverse_proxy http://localhost:${toString config.services.mms.port}
-        '';
-      };
-    })
+    # Redirect www.ony.world → ony.world
+    "www.ony.world" = {
+      extraConfig = ''
+        redir https://ony.world{uri} permanent
+      '';
+    };
+  };
+
+  music = {
+    "music.ony.world" = {
+      extraConfig = ''
+        reverse_proxy http://localhost:${toString config.services.mms.port}
+      '';
+    };
+  };
+
+  anlytics = {
+    "umami.ony.world" = {
+      extraConfig = ''
+        reverse_proxy http://localhost:${toString config.services.umami.settings.PORT}
+      '';
+    };
+  };
+in {
+  services.caddy.virtualHosts = lib.mkMerge [
+    website
+    anlytics
+    (lib.mkIf config.services.mms.enable music)
   ];
 }
