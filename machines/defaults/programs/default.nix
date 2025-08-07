8587c8bb6e23d9566b2fa3ad996adc54b5d3a373
Ony <onyrakoto27@gmail.com>
2025-08-07 13:30:33 +0300

    use umami as a service

--- machines/defaults/programs/default.nix
+++ machines/defaults/programs/default.nix
@@ -7,6 +7,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    openssl
     neofetch
   ];
 }
--- machines/host/hizuru/services/umami.nix
+++ machines/host/hizuru/services/umami.nix
@@ -1,5 +1,9 @@
 {
   services.umami = {
     enable = true;
+
+    settings = {
+      APP_SECRET_FILE = "/run/secrets/umami-app-secret";
+    };
   };
 }
