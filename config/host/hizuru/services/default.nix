4bed6b223ff9bc132c0027cff9a3f027c7e3e93e
Ony <onyrakoto27@gmail.com>
2025-08-07 22:08:28 +0300

    test webhook

--- config/host/hizuru/services/default.nix
+++ config/host/hizuru/services/default.nix
@@ -4,5 +4,6 @@
     ./umami.nix
     ./caddy.nix
     ./ony-world.nix
+    ./webhook.nix
   ];
 }
--- config/host/hizuru/services/webhook.nix
+++ config/host/hizuru/services/webhook.nix
@@ -1,12 +1,24 @@
-let
+{config, ...}: let
   hooks = {
     redeploy = {
       id = "redeploy-webhook";
       execute-command = "make update-local";
       command-working-directory = "/srv/os-config";
+
+      trigger-rule = {
+        match = {
+          type = "payload-hmac-sha1";
+          secret = builtins.readFile config.age.secrets.gh-hooks.path;
+          parameter = {
+            source = "header";
+            name = "X-Hub-Signature";
+          };
+        };
+      };
     };
   };
 in {
+  age.secrets.gh-hooks.file = ../secrets/gh-hooks.age;
   services.webhook = {
     enable = true;
     port = 9000;
