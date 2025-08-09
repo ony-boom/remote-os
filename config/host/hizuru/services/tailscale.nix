5ef85631d1994eb822b58c9010c028f87144103b
Ony <onyrakoto27@gmail.com>
2025-08-09 19:10:54 +0300

    with tailscale magic dns

--- config/host/hizuru/services/tailscale.nix
+++ config/host/hizuru/services/tailscale.nix
@@ -1,5 +1,10 @@
-{
+{lib, config, ...}: {
   services.tailscale = {
     enable = true;
+  };
+} // lib.mkIf (config.services.tailscale.enable) {
+  networking = {
+    nameServers = ["100.100.100.100" "8.8.8.8" "1.1.1.1"];
+    search = ["tempel-goblin.ts.net"];
   };
 }
