146a97cdc912d71a948f130e9f2084abbe34544e
Ony <ony@ony.world>
2025-11-01 22:46:25 +0300

    umami caddied

--- config/host/hizuru/services/caddy.nix
+++ config/host/hizuru/services/caddy.nix
@@ -1,9 +1,11 @@
 {
   inputs,
   pkgs,
+  config,
   ...
 }: let
   ony-world = inputs.ony-world.packages.${pkgs.system}.default;
+  umami-settings = config.services.umami.settings;
 in {
   services.caddy.virtualHosts = {
     "ony.world" = {
@@ -15,6 +17,12 @@ in {
     "www.ony.world" = {
       extraConfig = ''
         redir https://ony.world{uri}
+      '';
+    };
+
+    "umami.ony.world" = {
+      extraConfig = ''
+        reverse_proxy http://${umami-settings.HOSTNAME}:${umami-settings.PORT}
       '';
     };
   };
