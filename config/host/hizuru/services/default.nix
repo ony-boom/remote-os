794dc8593cae1178ce04f1cd6b45a101284cfcc6
Ony <ony@ony.world>
2025-12-27 09:01:18 +0300

    disable simple cd for now

--- config/host/hizuru/services/default.nix
+++ config/host/hizuru/services/default.nix
@@ -5,7 +5,7 @@
     ./tailscale.nix
     ./umami.nix
     ./copyparty.nix # file server
-    ./simple-cd.nix # webhook listener for auto deployment
+    # ./simple-cd.nix # webhook listener for auto deployment
 
     # docker/podman containers as services
     ./containers
