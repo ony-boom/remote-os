f878f297f77ee09e617f7f5be44cede2dc00e900
Ony <ony@ony.world>
2025-12-28 18:23:45 +0300

    With better webhook service path

--- config/host/hizuru/services/webhooks/default.nix
+++ config/host/hizuru/services/webhooks/default.nix
@@ -15,8 +15,20 @@
     after = ["network.target"];
     wantedBy = ["multi-user.target"];
 
+    path = with pkgs; [
+      bash
+      git
+      nix
+      gnumake
+      sudo
+      coreutils
+    ];
+
     serviceConfig = {
       ExecStart = "${lib.getExe pkgs.webhook} -ip 127.0.0.1 -template -verbose -hooks /etc/webhooks/hooks";
+      # Environment = [
+      #   "PATH=/run/current-system/sw/bin"
+      # ];
       EnvironmentFile = [
         config.age.secrets.webhooks.path
       ];
