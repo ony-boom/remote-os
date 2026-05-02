07762f3681281a8e3021bcbfea05a001951318c9
Ony <ony@ony.world>
2026-05-02 22:19:46 +0300

    fix typo

--- config/host/hizuru/services/woodpecker.nix
+++ config/host/hizuru/services/woodpecker.nix
@@ -3,8 +3,9 @@
   services.woodpecker-server = {
     enable = true;
     environment = {
-      WOODPECKER_HOST = "https://ci.ony.word";
+      WOODPECKER_HOST = "https://ci.ony.world";
       WOODPECKER_SERVER_ADDR = ":3007";
+      WOODPECKER_GRPC_ADDR = ":3008";
       WOODPECKER_OPEN = "true";
       WOODPECKER_GITHUB = "true";
     };
