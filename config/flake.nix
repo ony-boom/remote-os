ec47cfed1928fbaf6a2c4f9ae41d03a02fb0ff2e
Ony <ony@ony.world>
2026-05-06 05:37:19 +0300

    flake update

--- config/flake.nix
+++ config/flake.nix
@@ -29,6 +29,11 @@
     ];
   };
 
+  nixConfig = {
+    substituters = [" https://colmena.cachix.org"];
+    trusted-public-keys = ["colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="];
+  };
+
   outputs = {
     nixpkgs,
     disko,
